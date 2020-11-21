9a23fa50c4c6baf258db4dd0f4de637986f56f0e
martykeyima <73689767+martykeyima@users.noreply.github.com>
2020-11-21 10:32:12 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -22,11 +22,11 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table width="600" border="1" class = "table table-striped">
     <thead>
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-  </tr>
+        <tr>
+            <th width="100"> <div align="center">Name</div></th>
+            <th width="350"> <div align="center">Comment </div></th>
+            <th width="150"> <div align="center">Link </div></th>
+        </tr>
     </thead>
 <?php
 while($Result = mysqli_fetch_array($res))
