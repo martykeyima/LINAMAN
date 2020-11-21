a21fa628a286e5b83fac5afc49589fc9c8c92ecc
martykeyima <73689767+martykeyima@users.noreply.github.com>
2020-11-21 10:43:34 +0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+mysqli_real_connect($conn, 'databasecommy01.mysql.database.azure.com', 'MasterM@databasecommy01', 'Mm093493', 'Database_Gear', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
