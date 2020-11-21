00c54c7e6572b095dfee21baa4b557d53560d7d0
martykeyima <73689767+martykeyima@users.noreply.github.com>
2020-11-21 10:30:36 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -21,7 +21,7 @@ if (mysqli_connect_errno($conn))
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table width="600" border="1" class = "table table-striped">
-    <thead class="thead-dark">
+    <thead>
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
